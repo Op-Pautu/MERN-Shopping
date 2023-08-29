951fd10d592c3c63cc458958bc46deb5c9732726
Op-Pautu <pautuop@gmail.com>
2023-08-29 14:54:54 +0530

    Initial commit plus add styled-components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { styled } from "styled-components"
+
+const Container = styled.div`
+    height: 60px;
+    background-color: black;
+`
+
+const Navbar = () => {
+  return (
+    <Container>Navbar</Container>
+  )
+}
+
+export default Navbar
