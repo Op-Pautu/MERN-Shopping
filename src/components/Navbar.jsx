ace23bce50beafdbbebf53faef7722fe1f1b9415
Op-Pautu <pautuop@gmail.com>
2023-08-30 12:53:41 +0530

    Finish navbar v1 and add announcement

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,84 @@
-import { styled } from "styled-components"
+
+import { styled } from "styled-components";
+import { AiOutlineSearch, AiOutlineShoppingCart } from 'react-icons/ai';
 
 const Container = styled.div`
-    height: 60px;
-    background-color: black;
+  height: 60px;
+`;
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+const SearchContainer = styled.div`
+  border: 1px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+  gap:10px;
+  
+`
+const Input = styled.input`
+  border: none;
+`
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+`
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 25px;
+ 
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  
 `
 
 const Navbar = () => {
   return (
-    <Container>Navbar</Container>
-  )
-}
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+           <Input/>
+           <AiOutlineSearch style={{ color: 'gray', fontSize: 16}}/>
+           
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>OP.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem><AiOutlineShoppingCart/> </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
 
-export default Navbar
+export default Navbar;
