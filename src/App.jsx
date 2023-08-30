5d1efd96301cee3bc29a51f9a63207cbc4670bf5
Op-Pautu <pautuop@gmail.com>
2023-08-31 01:02:52 +0530

    Add Product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
+
 import Home from './pages/Home'
+import Product from './pages/Product'
 import ProductList from './pages/ProductList'
 
 function App() {
@@ -8,7 +10,7 @@ function App() {
   return (
     <>
       {/* <Home/> */}
-      <ProductList/>
+      <Product/>
     </>
   )
 }
