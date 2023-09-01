52d9c6a3208098f740bec8eafdfbde04f46d5fec
Op-Pautu <pautuop@gmail.com>
2023-09-01 15:24:31 +0530

    Add get and delete operations in user

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { verifyToken, verifyTokenAndAuthorization } = require('./verifyToken')
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require('./verifyToken')
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
@@ -11,6 +11,30 @@ const hashPassword = async (password) => {
     const hashedPassword = await bcrypt.hash(password, salt);
     return hashedPassword;
   };
+
+// GET USER
+router.get("/find/:id", verifyTokenAndAdmin, async(req,res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        const {password, ...others}  = user._doc
+        res.status(200).json(others)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})  
+
+// GET ALL USERS
+router.get("/", verifyTokenAndAdmin, async(req,res) => {
+    const query = req.query.new
+    try {
+        const users = query 
+        ? await User.find().sort({_id: -1}).limit(5)
+        : await User.find()
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})  
 
 router.put("/:id", verifyTokenAndAuthorization, async(req, res) => {
     if (req.body.password) {
@@ -30,5 +54,14 @@ router.put("/:id", verifyTokenAndAuthorization, async(req, res) => {
    }
 })
 
+// DELETE
+router.delete("/:id", verifyTokenAndAdmin, async(req,res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json("User has been deleted...")
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
 module.exports = router
